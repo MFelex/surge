d86076bd60092acac5e0e2459f692e6ca25d0624
Mahdi <mehdifelex@gmail.com>
2021-12-23 18:05:38 +0330

    WIP: edit docker compose and some refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $1 == "uwsgi" ]; then
-    shift
-    /usr/bin/uwsgi --uid www-data --gid www-data --plugins=python3 --chdir=/var/www --socket=0.0.0.0:8000 $@
+        shift
+        /usr/bin/uwsgi --uid www-data --gid www-data --plugins=python3 --chdir=/var/www --socket=0.0.0.0:8000 $@
 elif [ $1 == "worker" ]; then
     shift
     celery $@
 elif [ $1 == "bash" ]; then
-    bash
+        bash
 fi
