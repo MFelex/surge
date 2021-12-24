4d7ac547f51d8955a36a7b272dad71c4edefa0fc
Mahdi <mehdifelex@gmail.com>
2021-12-24 18:53:42 +0330

    FIX: change approach of gdal container to be a Dockerfile seperatly

--- postgis-data/import-script.sh
+++ postgis-data/import-script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-cd /tmp|| echo "Directory Not Found"
+cd /postgis-data|| echo "Directory Not Found"
 export PGPASSWORD="$POSTGRES_PASSWORD"; psql -U "$POSTGRES_USER" -h postgres -d "$POSTGRES_DB" -f pscript.sql
 ogr2ogr --debug ON -f PostgreSQL PG:"host=postgres port=5432 user=$POSTGRES_USER dbname=$POSTGRES_DB password=$POSTGRES_PASSWORD" -update -overwrite -select name,name:en -lco GEOMETRY_NAME=geometry -lco FID=gid -nln districts districts.geojson
